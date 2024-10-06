5938bbcf9e08206d9a5994a344e7105a17a16e6b
Chaerin Hwang <chaa00@naver.com>
2024-10-06 15:24:06 +0900

    initial commit

---
+++ src/main/java/crhwang0822/toyproject/chattingpractice/ChattingPracticeApplication.java
@@ -0,0 +1,13 @@
+package crhwang0822.toyproject.chattingpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/crhwang0822/toyproject/chattingpractice/ChattingPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package crhwang0822.toyproject.chattingpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
